8ed5757bb611a9fc7e9c1213d733248cda41aef4
YenTingWu <a9600125a@gmail.com>
2022-01-24 11:56:10 +0800

    chore: init chakraui

---
+++ fof-questionnaire/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  fof: {
+    black: '#333333',
+    dark_gray: '#555555',
+    light_gray: '#888888',
+    main: '#2D3748',
+    secondary: '#A0AEC0',
+    blue: '#4299E1',
+    light_red: '#FFD6D6',
+  },
+};
+
+export default extendTheme({ colors });
